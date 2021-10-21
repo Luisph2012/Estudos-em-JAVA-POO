496fa9a5284d4f3456071534ec844565f087ef86
Luis Philipe Vilara Ribeiro <89031690+Luisph2012@users.noreply.github.com>
2021-10-20 21:34:07 -0300

    Primeira Versão

---
+++ If e Else.java
@@ -0,0 +1,12 @@
+public class MyClass { //forma de declaracao
+	public static void main(String args[]) { //funcao principal
+		int ano = 2012; //variavel com valor .. a variavel tem o nome "ano" :D
+		if(ano%4 == 0)
+	{ //estrutura de decisao, vendo que para ser bissexto tem que ser divisivel por 4 entao o resto tem que ser 0 nos valores
+			que dividem por 4
+			System.out.println("O ano " + ano + " é Bissexto");// se o valor for divisivel ele vai escrever isso
+		}else{// se a primeira nao validar o programa vai validar esse
+System.out.println("O ano " + ano + " não é Bissexto");//ele vai escrever numeros nao dividem por 4
+}
+}
+}
